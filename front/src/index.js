46e8fb6bf9a9507818bf1f1866ae0c14d404f01c
ispret9110@gmail.com <ispret9110@gmail.com>
2024-01-09 18:23:53 +0900

    init

--- front/src/index.js
+++ front/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import axios from "axios";
 
-// axios.defaults.baseURL = "http://localhost:3000";
-axios.defaults.baseURL = "https://jungle-board-server.onrender.com";
+axios.defaults.baseURL = "http://localhost:3000";
+// axios.defaults.baseURL = "https://jungle-board-server.onrender.com";
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
